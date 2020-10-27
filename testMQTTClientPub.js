3bac289e2f011d6dad42fee39427440ad05126b0
Strypper <ocgrb.strypperjason115@gmail.com>
2020-10-27 21:14:20 +0700

    Test change

--- testMQTTClientPub.js
+++ testMQTTClientPub.js
@@ -1,7 +1,7 @@
 var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://localhost:1883')
 var topic = 'TrashChannel'
-var message = 'Hello world !'
+var message = 'Hello world from the cloud !'
 
 client.on('connect', () => {
     setInterval(() => {
